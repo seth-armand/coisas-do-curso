7a81a800402704724224631406f3504413639174
enzo <mestacherenotu@gmail.com>
2024-06-14 22:08:20 -0300

    first commit

---
+++ src/main/java/primeira/lista/lista/ListaApplication.java
@@ -0,0 +1,16 @@
+package primeira.lista.lista;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ListaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ListaApplication.class, args);
+	
+	System.out.println("escreva o primeiro numero ");
+	
+	}
+
+}
