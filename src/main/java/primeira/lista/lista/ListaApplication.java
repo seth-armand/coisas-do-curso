6f256e755eed59e735ef5e9262a33bfe17df3fa8
enzo <mestacherenotu@gmail.com>
2024-06-18 17:22:34 -0300

    primeiro exercicio

--- src/main/java/primeira/lista/lista/ListaApplication.java
+++ src/main/java/primeira/lista/lista/ListaApplication.java
@@ -8,9 +8,17 @@ public class ListaApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ListaApplication.class, args);
-	
-	System.out.println("escreva o primeiro numero ");
-	
+
+		Scanner S = new scanner(System.in);
+		System.out.println("escreva o primeiro numeor ");
+		double numero1 = S.nextdouble(System.in);
+		System.out.println("escreva o segundo numero ");
+		double numero2 = S.nextdouble(System.in);
+		S.close();
+		double soma = numero1 + numero2;
+		System.out.println("a soma de" + numero1 + "e" + numero2 + "igua a " + soma);
+
+
 	}
 
 }
